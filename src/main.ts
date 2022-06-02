536a7280889f4ffde59ac1a70cbdc87525019b4f
SpectralGT <atharvsingh1396@gmail.com>
2022-06-02 13:41:10 +0530

    created a canvas

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
-import './style.css'
+export {};
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+function main() {
+	const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+	const gl = canvas.getContext("webgl") as WebGLRenderingContext;
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+	if (gl == null) {
+		window.alert("WebGL not Supported");
+	}
+
+	// Set clear color to black, fully opaque
+	gl.clearColor(0.0, 0.0, 0.0, 1.0);
+	// Clear the color buffer with specified clear color
+	gl.clear(gl.COLOR_BUFFER_BIT);
+}
+window.onload = main;
